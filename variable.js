3ea1ca1c767d028c5518708a1139f9acd17da4b8
Bluerana377 <hasansohel492@gmail.com>
2023-08-06 12:45:57 +0400

    added the variable file

---
+++ variable.js
@@ -0,0 +1,32 @@
+// Number Type variable
+var sunglassPrice = 300;
+
+// String type variable
+var specialOneName = 'hande ercel';
+
+// boolean type variable
+var isSingle = true;
+// two type of number: Integer: 120,
+// float decimal: 3.65
+
+// variable name convention: rules. best practice
+
+/**
+ * single word
+ * no gap ore space
+ * no quote 
+ * no keyword
+ * can not start with a number
+ * can not contain dash
+ * should use camelCase 
+*/
+
+// Math operation: +, -, *, /
+//Shorthand: +=, -=, ++, --
+
+// convert: parseInt, parseFloat
+
+// to fixed
+
+// reminder 'vagses er jonneya' %
+ 
