28db372cdf03df6d570db3ed1e0a789f050a1ddd
Bluerana377 <hasansohel492@gmail.com>
2023-08-06 17:17:08 +0400

    added the index.js file

---
+++ array.js
@@ -0,0 +1,5 @@
+var friendAge = [23, 24, 72, 34, 23];
+var picnicFee = [5000, 3000, 4000, 200,];
+var naikas = ['mahiya mahi', 'opu biswash', 'Shabnur', 'purnima'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+ 
---
+++ index.js
@@ -0,0 +1,16 @@
+var numbers = [36, 45, 65, 56, 46];
+
+// 1. get element by index
+var element = numbers[1];
+// console.log(element);
+// 2. set element value by index
+numbers[1] = 77;
+numbers[3] = 44;
+// console.log(numbers);
+
+// 3. find index of an element 
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex);
+
+
+
